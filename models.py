fdb88b945d37c57e0a203ca8d41579d8e8651ae6
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-11-20 15:01:08 -0800

    app.py routes

---
+++ models.py
@@ -0,0 +1,42 @@
+import datetime
+from peewee import * 
+from flask_login import UserMixin 
+
+Database = SqliteDatabase('locations.sqlite')
+
+class User(db.Model, UserMixin):
+    __tableName__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.CharField(50), nullable=False, unique=True)
+    email = db.Column(db.CharField(50), nullable=False, unique=True)
+    password = db.Column(db.CharField(255), nullable=False)
+    user_pic = db.Column(db.CharField(255))
+    
+    class meta: 
+        database = DATABASE 
+        
+class Location(Model):
+    __tablename__ = "locations"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    loc_name: db.Column(db.CharField(), unique=True)
+    street_num: db.Column(db.IntegerField(6))
+    street_name: db.Column(db.CharField())
+    apt_unit_num: db.Column(db.IntegerField())
+    city: db.Column(db.CharField())
+    State: db.Column(db.CharField(2))
+    zipcode: db.Column(db.IntegerField(5))
+    ada: db.Column(BooleanField())
+    unisex: db.Column(BooleanField())
+    open_time: db.Column(db.CharField())
+    closing_time: db.Column(db.CharField())
+    Directions: db.Column(db.CharField(255))
+
+def initialize(): 
+    DATABASE.connect()
+    DATABASE.create_tables([User, Location], safe=True)
+    print("Tables Created")
+    DATABASE.close()
+         
+
+     
--- resources/user.py
+++ resources/user.py
@@ -8,3 +8,68 @@ from flask_login import login_user,
 current_user
 from playhouse.shortcuts import
 model_to_dict
+
+##setting up the user & login
+
+user = Blueprint("users", user)
+
+@user.route('/register', methods= ["POST"])
+def register():
+    payload = request.get_json()
+
+    payload['email'].lower()
+    try:
+        #does user exist in db?
+        models.User.get
+        (modesl.User.email == payload ['email'])
+        #finding user by email
+
+        return jsonify(data={},
+        status={"code": 401,
+        "message": "User with that Email and name already exits"})
+        except models.DoesNotExist:
+            payload["password"] =
+            generate_password_hash
+            (payload["password"])
+            #bcrypt for hash
+            user = modesl.User.create
+            (**payload)
+
+            #login portion
+            login_user(user)
+
+            user_dict = model_to_dict
+            (user)
+            print(user_dict)
+            print(type(user_dict["password"]))
+
+            return jsonify(data=user_dict, status= {"code": 201, "message": "It worked"})
+
+@user.route('/login', methods=["POST"])
+def login():
+payload = request.get_json()
+print(payload)
+try: 
+    user = models.User.get(models.User.email == payload ["email"])
+    user_dict = model_to_dict(user)
+    if(check_password_hash(user_dict["password"],
+    payload['password']))
+    del user_dict['password']
+    login_user(user)
+    print('Welcome back ' + user)
+        return jsonify(data=user_dict, status={"code: 200", "message": "Success"})
+    else:
+        return jsonify(data={}, status={"code": 401, "message": "Email or Password is incorrect"})
+
+@user.route('/edit', methods=["EDIT"])
+def edit_user():
+    query = models.user.edit().where(edit.User.user(id==id)
+    query.execute()
+    return jsonify(data='resource successfully edited', status={"code": 200, "message": resource successfully edited}))
+
+@user.route('/id', methods=["DELETE"])
+def delete_user(id):
+    query = models.user.delete().where(models.User.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status={"code": 200, "message": "resource deleted successfully"})
+
