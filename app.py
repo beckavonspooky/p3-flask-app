19bfd0cc3988e16f7bae7edc34e35fb33f76a38c
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-11-20 13:09:10 -0800

    routes created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 DEBUG = True
 PORT = 8000
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'This Is Working!'
+
+@app.route('/json')
+def location():
+    return jsonify(name="Starbucks", address=8)
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
