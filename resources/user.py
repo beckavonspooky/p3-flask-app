23b1f6445d32555e63c5e4ff92cebd1caaf729e7
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-11-20 16:09:08 -0800

    fixed tabbing errors in user.py

--- resources/user.py
+++ resources/user.py
@@ -1,13 +1,9 @@
 import models
 
-from flask import request, jsonify,
-Blueprint
-from flask_bcrypt import
-generate_password_has, check_password_hash
-from flask_login import login_user,
-current_user
-from playhouse.shortcuts import
-model_to_dict
+from flask import request, jsonify, Blueprint
+from flask_bcrypt import generate_password_has, check_password_hash
+from flask_login import login_user, current_user
+from playhouse.shortcuts import model_to_dict
 
 ##setting up the user & login
 
@@ -19,27 +15,22 @@ def register():
 
     payload['email'].lower()
     try:
-        #does user exist in db?
-        models.User.get
-        (modesl.User.email == payload ['email'])
-        #finding user by email
+    #does user exist in db?
+    models.User.get
+    (modesl.User.email == payload ['email'])
+    #finding user by email
 
-        return jsonify(data={},
+     return jsonify(data={},
         status={"code": 401,
         "message": "User with that Email and name already exits"})
-        except models.DoesNotExist:
-            payload["password"] =
-            generate_password_hash
-            (payload["password"])
+        except models.DoesNotExist: payload["password"] = generate_password_hash (payload["password"])
             #bcrypt for hash
-            user = modesl.User.create
-            (**payload)
+            user = modesl.User.create (**payload)
 
             #login portion
             login_user(user)
 
-            user_dict = model_to_dict
-            (user)
+            user_dict = model_to_dict (user)
             print(user_dict)
             print(type(user_dict["password"]))
 
@@ -47,25 +38,24 @@ def register():
 
 @user.route('/login', methods=["POST"])
 def login():
-payload = request.get_json()
-print(payload)
-try: 
-    user = models.User.get(models.User.email == payload ["email"])
-    user_dict = model_to_dict(user)
-    if(check_password_hash(user_dict["password"],
-    payload['password']))
-    del user_dict['password']
-    login_user(user)
-    print('Welcome back ' + user)
-        return jsonify(data=user_dict, status={"code: 200", "message": "Success"})
-    else:
-        return jsonify(data={}, status={"code": 401, "message": "Email or Password is incorrect"})
+    payload = request.get_json()
+    print(payload)
+    try: 
+        user = models.User.get(models.User.email == payload ["email"])
+        user_dict = model_to_dict(user)
+        if(check_password_hash(user_dict["password"], payload['password']))
+            del user_dict['password']
+            login_user(user)
+            print('Welcome back ' + user)
+            return jsonify(data=user_dict, status={"code: 200", "message": "Success"})
+        else:
+            return jsonify(data={}, status={"code": 401, "message": "Email or Password is incorrect"})
 
 @user.route('/edit', methods=["EDIT"])
 def edit_user():
-    query = models.user.edit().where(edit.User.user(id==id)
+    query = models.user.edit().where(edit.User.userid==id)
     query.execute()
-    return jsonify(data='resource successfully edited', status={"code": 200, "message": resource successfully edited}))
+    return jsonify (data='resource successfully edited', status={"code": 200, "message": resource successfully edited}))
 
 @user.route('/id', methods=["DELETE"])
 def delete_user(id):
