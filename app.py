dc20ea30ea6f42b4b4f289b3b1b608df0034f102
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-11-25 17:50:46 -0800

    if fix

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ CORS(user, origin=['http://localhost:3000', 'http://gottagonow.heroku.app.com'],
 app.register_blueprint(user, url_prefix='/api/v1/users')
 app.register_blueprint(location, url_prefix='/api/v1/locations')
 
-'ON_HEROKU' in os.environ:
+if 'ON_HEROKU' in os.environ:
     print('hitting')
     models.initialize()
 
